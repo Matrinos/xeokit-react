f8f14599307680245ecb8362c4d05129e15822a0
Jiacheng Jiang <ajax15601560911@gmail.com>
2021-10-21 18:19:22 +0800

    chore: Code cleanup

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -17,16 +17,7 @@ import {
 } from '@tuxmart/xeokit-sdk';
 import { Camera } from '@tuxmart/xeokit-sdk/viewer/scene/camera/Camera';
 import { forEach, noop } from 'lodash';
-import {
-  FC,
-  forwardRef,
-  useCallback,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState,
-} from 'react';
-
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { drawAABB, get2dFrom3d } from './utils';
 
 interface Model {
@@ -115,10 +106,6 @@ export interface ViewerProps {
   onUnselectEntity?: () => void;
 }
 
-export interface RefProps {
-  unselect?: () => void;
-}
-
 interface ModelEntity extends Entity {
   on: (event: string, callback: (...args: unknown[]) => void) => void;
 }
@@ -126,305 +113,294 @@ interface ModelEntity extends Entity {
 export const makeViewer = (
   LoaderPlugin: typeof GLTFLoaderPlugin | typeof XKTLoaderPlugin,
 ): FC<ViewerProps> => {
-  const ModelViewer = forwardRef<RefProps, ViewerProps>(
-    (
-      {
-        canvasID,
-        width,
-        height,
-        models,
-        bcfViewpoint,
-        eventToPickOn = 'mouseclicked',
-        navCubeSettings,
-        plugins,
-        components,
-        isDev = false,
-        debug = false,
-        onUpdateXY = noop,
-        onSelectEntity = noop,
-        // onUnselectEntity,
-      },
-      forwardedRef,
-    ) => {
-      const [isDevPanelVisible, setIsDevPanelVisible] = useState(false);
-      const lineCanvas = useRef<HTMLCanvasElement>(null);
-      const bimCanvas = useRef<HTMLCanvasElement>(null);
-      const viewer = useRef<Viewer>();
-      const modelLoader = useRef<InstanceType<typeof LoaderPlugin>>();
-      const bcfViewpointsPlugin = useRef<BCFViewpointsPlugin>();
-      const storeyViewsPlugin = useRef<StoreyViewsPlugin>();
-      const event = useRef<string>();
+  const ModelViewer: FC<ViewerProps> = ({
+    canvasID,
+    width,
+    height,
+    models,
+    bcfViewpoint,
+    eventToPickOn = 'mouseclicked',
+    navCubeSettings,
+    plugins,
+    components,
+    isDev = false,
+    debug = false,
+    onUpdateXY = noop,
+    onSelectEntity = noop,
+  }) => {
+    const [isDevPanelVisible, setIsDevPanelVisible] = useState(false);
+    const lineCanvas = useRef<HTMLCanvasElement>(null);
+    const bimCanvas = useRef<HTMLCanvasElement>(null);
+    const viewer = useRef<Viewer>();
+    const modelLoader = useRef<InstanceType<typeof LoaderPlugin>>();
+    const bcfViewpointsPlugin = useRef<BCFViewpointsPlugin>();
+    const storeyViewsPlugin = useRef<StoreyViewsPlugin>();
+    const event = useRef<string>();
 
-      const unselect = noop;
-      useImperativeHandle(forwardedRef, () => ({
-        unselect,
-      }));
+    useEffect(() => {
+      return () => {
+        if (viewer.current) {
+          viewer.current.destroy();
+        }
+      };
+    }, []);
 
-      useEffect(() => {
-        return () => {
-          if (viewer.current) {
-            viewer.current.destroy();
-          }
-        };
-      }, []);
+    const setUpViewer = useCallback(() => {
+      viewer.current = new Viewer({
+        canvasId: canvasID,
+        saoEnabled: true,
+      });
 
-      const setUpViewer = useCallback(() => {
-        viewer.current = new Viewer({
-          canvasId: canvasID,
-          saoEnabled: true,
-        });
+      viewer.current.scene.pointsMaterial.roundPoints = false;
 
-        viewer.current.scene.pointsMaterial.roundPoints = false;
-
-        modelLoader.current = new LoaderPlugin(viewer.current, {
-          objectDefaults: {
-            IfcOpeningElement: {
-              pickable: false,
-              visible: false,
-            },
-
-            IfcSpace: {
-              colorize: [0.5, 0.5, 1],
-              pickable: true,
-              visible: true,
-              opacity: 0.3,
-            },
-
-            IfcWindow: {
-              colorize: [0.137255, 0.403922, 0.870588],
-              opacity: 0.5,
-            },
-
-            IfcPlate: {
-              colorize: [0.8470588235, 0.427450980392, 0],
-              opacity: 0.3,
-            },
-
-            DEFAULT: {},
-            // IfcSpace: {
-            //   pickable: true,
-            //   opacity: 1,
-            // },
+      modelLoader.current = new LoaderPlugin(viewer.current, {
+        objectDefaults: {
+          IfcOpeningElement: {
+            pickable: false,
+            visible: false,
           },
-          maxGeometryBatchSize: 50000000,
+
+          IfcSpace: {
+            colorize: [0.5, 0.5, 1],
+            pickable: true,
+            visible: true,
+            opacity: 0.3,
+          },
+
+          IfcWindow: {
+            colorize: [0.137255, 0.403922, 0.870588],
+            opacity: 0.5,
+          },
+
+          IfcPlate: {
+            colorize: [0.8470588235, 0.427450980392, 0],
+            opacity: 0.3,
+          },
+
+          DEFAULT: {},
+          // IfcSpace: {
+          //   pickable: true,
+          //   opacity: 1,
+          // },
+        },
+        maxGeometryBatchSize: 50000000,
+      });
+      new FastNavPlugin(viewer.current, {});
+    }, [canvasID]);
+
+    const setCamera = useCallback(() => {
+      // if (camera) {
+      //   const { eye, look, up } = camera;
+      //   camera.eye = eye;
+      //   camera.look = look;
+      //   camera.up = up;
+      // }
+      // (viewer.current?.camera as Camera).projection = 'ortho';
+    }, []);
+
+    const setBCFViewpoints = useCallback(() => {
+      bcfViewpointsPlugin.current?.setViewpoint(bcfViewpoint);
+    }, [bcfViewpoint]);
+
+    const loadModels = useCallback(async () => {
+      const entities = models.map((model) => {
+        const m = modelLoader.current?.load({
+          ...model,
+          edges: true,
+          performance: false,
+          excludeUnclassifiedObjects: true,
+        }) as ModelEntity;
+
+        return m;
+      });
+      await Promise.all(
+        entities.map((model) => new Promise((resolve) => model.on('loaded', resolve))),
+      );
+    }, [models]);
+
+    const loadPlugins = useCallback(
+      (viewerObj: Viewer) => {
+        forEach(plugins, (plugin) => {
+          const [, ...restArgs] = plugin.args || [];
+          new plugin.plugin(viewerObj, ...restArgs);
         });
-        new FastNavPlugin(viewer.current, {});
-      }, [canvasID]);
 
-      const setCamera = useCallback(() => {
-        // if (camera) {
-        //   const { eye, look, up } = camera;
-        //   camera.eye = eye;
-        //   camera.look = look;
-        //   camera.up = up;
-        // }
-        // (viewer.current?.camera as Camera).projection = 'ortho';
-      }, []);
-
-      const setBCFViewpoints = useCallback(() => {
-        bcfViewpointsPlugin.current?.setViewpoint(bcfViewpoint);
-      }, [bcfViewpoint]);
-
-      const loadModels = useCallback(async () => {
-        const entities = models.map((model) => {
-          const m = modelLoader.current?.load({
-            ...model,
-            edges: true,
-            performance: false,
-            excludeUnclassifiedObjects: true,
-          }) as ModelEntity;
-
-          return m;
+        forEach(components, (component) => {
+          const [, ...restArgs] = component.args || [];
+          new component.component(viewerObj.scene, ...restArgs);
         });
-        await Promise.all(
-          entities.map((model) => new Promise((resolve) => model.on('loaded', resolve))),
-        );
-      }, [models]);
 
-      const loadPlugins = useCallback(
-        (viewerObj: Viewer) => {
-          forEach(plugins, (plugin) => {
-            const [, ...restArgs] = plugin.args || [];
-            new plugin.plugin(viewerObj, ...restArgs);
+        if (isDev) {
+          new TreeViewPlugin(viewerObj, {
+            containerElement: document.getElementById('treeViewContainer'),
           });
+        }
+      },
+      [components, isDev, plugins],
+    );
 
-          forEach(components, (component) => {
-            const [, ...restArgs] = component.args || [];
-            new component.component(viewerObj.scene, ...restArgs);
-          });
+    const pickEntity = useCallback(() => {
+      let lastEntity: Entity | undefined;
+      let lastColorize: Colorize;
 
-          if (isDev) {
-            new TreeViewPlugin(viewerObj, {
-              containerElement: document.getElementById('treeViewContainer'),
+      const scene = viewer.current?.scene;
+      const cam = viewer.current?.camera as Camera;
+      event.current && cam.off(event.current);
+
+      scene?.input.on(eventToPickOn, (coords: [number, number]) => {
+        const hit = scene.pick({
+          canvasPos: coords,
+        });
+
+        if (hit) {
+          if (!lastEntity || hit.entity.id !== lastEntity.id) {
+            if (lastEntity) {
+              lastEntity.colorize = lastColorize;
+            }
+            lastEntity = hit.entity;
+            lastColorize = hit.entity.colorize.slice();
+            hit.entity.colorize = [0.0, 1.0, 1.0];
+
+            const ett = hit.entity as Entity;
+            onSelectEntity(ett.id);
+            const aabb = ett.aabb;
+            const center = [
+              (aabb[3] + aabb[0]) / 2,
+              (aabb[4] + aabb[1]) / 2,
+              (aabb[5] + aabb[2]) / 2,
+            ];
+            event.current = cam.on('matrix', (e: number[]) => {
+              const bimCtx = lineCanvas.current?.getContext('2d');
+
+              if (bimCtx) {
+                if (debug) drawAABB(bimCtx, [...e], [...aabb]);
+              }
+              const [x, y] = get2dFrom3d(width, height, [...e], center);
+              onUpdateXY({ x, y });
             });
           }
-        },
-        [components, isDev, plugins],
-      );
-
-      const pickEntity = useCallback(() => {
-        let lastEntity: Entity | undefined;
-        let lastColorize: Colorize;
-
-        const scene = viewer.current?.scene;
-        const cam = viewer.current?.camera as Camera;
-        event.current && cam.off(event.current);
-
-        scene?.input.on(eventToPickOn, (coords: [number, number]) => {
-          const hit = scene.pick({
-            canvasPos: coords,
-          });
-
-          if (hit) {
-            if (!lastEntity || hit.entity.id !== lastEntity.id) {
-              if (lastEntity) {
-                lastEntity.colorize = lastColorize;
-              }
-              lastEntity = hit.entity;
-              lastColorize = hit.entity.colorize.slice();
-              hit.entity.colorize = [0.0, 1.0, 1.0];
-
-              const ett = hit.entity as Entity;
-              onSelectEntity(ett.id);
-              const aabb = ett.aabb;
-              const center = [
-                (aabb[3] + aabb[0]) / 2,
-                (aabb[4] + aabb[1]) / 2,
-                (aabb[5] + aabb[2]) / 2,
-              ];
-              event.current = cam.on('matrix', (e: number[]) => {
-                const bimCtx = lineCanvas.current?.getContext('2d');
-
-                if (bimCtx) {
-                  if (debug) drawAABB(bimCtx, [...e], [...aabb]);
-                }
-                const [x, y] = get2dFrom3d(width, height, [...e], center);
-                onUpdateXY({ x, y });
-              });
-            }
-          } else if (lastEntity) {
-            lastEntity.colorize = lastColorize;
-            lastEntity = undefined;
-            onSelectEntity();
-          }
-        });
-      }, [debug, eventToPickOn, height, onSelectEntity, onUpdateXY, width]);
-
-      useEffect(() => {
-        (async () => {
-          setUpViewer();
-          setCamera();
-          // (viewer.current?.camera as Camera).projection = 'ortho';
-          viewer.current && loadPlugins(viewer.current);
-          await loadModels();
-          if (bcfViewpoint) setBCFViewpoints();
-          pickEntity();
-        })();
-      }, [
-        bcfViewpoint,
-        loadModels,
-        loadPlugins,
-        pickEntity,
-        setBCFViewpoints,
-        setCamera,
-        setUpViewer,
-      ]);
-
-      const toggleDevPanel = useCallback(() => {
-        setIsDevPanelVisible((prev) => !prev);
-      }, []);
-
-      const takeScreenshot = useCallback(() => {
-        // const imageData = viewer.current?.getSnapshot({
-        //   format: 'png',
-        // });
-
-        // if (imageData) {
-        //   const link = document.createElement('a');
-        //   link.setAttribute('href', imageData);
-        //   link.setAttribute('download', 'model-screenshot');
-        //   link.click();
-        // }
-
-        const mp = storeyViewsPlugin.current?.createStoreyMap('3P3Lub7Zj769ajSMElBcQ3', {
-          width: 1920,
-          height: 1920,
-        });
-        const imageData = mp?.imageData;
-
-        if (imageData) {
-          const link = document.createElement('a');
-          link.setAttribute('href', imageData);
-          link.setAttribute('download', 'storey-map');
-          link.click();
+        } else if (lastEntity) {
+          lastEntity.colorize = lastColorize;
+          lastEntity = undefined;
+          onSelectEntity();
         }
-      }, []);
+      });
+    }, [debug, eventToPickOn, height, onSelectEntity, onUpdateXY, width]);
 
-      const downloadBCF = useCallback(() => {
-        const viewpoint = bcfViewpointsPlugin.current?.getViewpoint({
-          // Options
-          spacesVisible: true, // Don't force IfcSpace types visible in viewpoint (default)
-          spaceBoundariesVisible: true, // Don't show IfcSpace boundaries in viewpoint (default)
-          openingsVisible: false, // Don't force IfcOpening types visible in viewpoint (default)
-        });
+    useEffect(() => {
+      (async () => {
+        setUpViewer();
+        setCamera();
+        // (viewer.current?.camera as Camera).projection = 'ortho';
+        viewer.current && loadPlugins(viewer.current);
+        await loadModels();
+        if (bcfViewpoint) setBCFViewpoints();
+        pickEntity();
+      })();
+    }, [
+      bcfViewpoint,
+      loadModels,
+      loadPlugins,
+      pickEntity,
+      setBCFViewpoints,
+      setCamera,
+      setUpViewer,
+    ]);
 
-        const viewpointStr = JSON.stringify(viewpoint, null, 2);
+    const toggleDevPanel = useCallback(() => {
+      setIsDevPanelVisible((prev) => !prev);
+    }, []);
 
+    const takeScreenshot = useCallback(() => {
+      // const imageData = viewer.current?.getSnapshot({
+      //   format: 'png',
+      // });
+
+      // if (imageData) {
+      //   const link = document.createElement('a');
+      //   link.setAttribute('href', imageData);
+      //   link.setAttribute('download', 'model-screenshot');
+      //   link.click();
+      // }
+
+      const mp = storeyViewsPlugin.current?.createStoreyMap('3P3Lub7Zj769ajSMElBcQ3', {
+        width: 1920,
+        height: 1920,
+      });
+      const imageData = mp?.imageData;
+
+      if (imageData) {
         const link = document.createElement('a');
-        link.setAttribute(
-          'href',
-          'data:text/plain;charset=utf-8,' + encodeURIComponent(viewpointStr),
-        );
-        link.setAttribute('download', 'bcfViewpoint.json');
+        link.setAttribute('href', imageData);
+        link.setAttribute('download', 'storey-map');
         link.click();
-      }, []);
+      }
+    }, []);
 
-      return (
-        <Container width={width} height={height}>
-          <canvas ref={bimCanvas} id={canvasID} width={width} height={height} />
-          {navCubeSettings ? (
-            <canvas
-              style={{
-                position: 'absolute',
-                right: 0,
-                bottom: 0,
-              }}
-              id={navCubeSettings.canvasId}
-              width={navCubeSettings.canvasWidth}
-              height={navCubeSettings.canvasHeight}
-            />
-          ) : null}
+    const downloadBCF = useCallback(() => {
+      const viewpoint = bcfViewpointsPlugin.current?.getViewpoint({
+        // Options
+        spacesVisible: true, // Don't force IfcSpace types visible in viewpoint (default)
+        spaceBoundariesVisible: true, // Don't show IfcSpace boundaries in viewpoint (default)
+        openingsVisible: false, // Don't force IfcOpening types visible in viewpoint (default)
+      });
 
-          {isDev && (
-            <>
-              <DevPanel isDevPanelVisible={isDevPanelVisible}>
-                <TreeViewContainer id="treeViewContainer" />
-                <Buttons>
-                  <IconButton
-                    type="button"
-                    id="take-screenshot"
-                    className="btn btn-primary"
-                    onClick={takeScreenshot}
-                  >
-                    <PhotoCameraIcon />
-                  </IconButton>
-                  <IconButton onClick={downloadBCF}>
-                    <SaveIcon />
-                  </IconButton>
-                </Buttons>
-              </DevPanel>
-              <IconButton
-                onClick={toggleDevPanel}
-                style={{ position: 'absolute', left: 10, top: 10 }}
-              >
-                {isDevPanelVisible ? <CloseIcon /> : <MenuIcon />}
-              </IconButton>
-            </>
-          )}
-        </Container>
+      const viewpointStr = JSON.stringify(viewpoint, null, 2);
+
+      const link = document.createElement('a');
+      link.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(viewpointStr),
       );
-    },
-  );
+      link.setAttribute('download', 'bcfViewpoint.json');
+      link.click();
+    }, []);
+
+    return (
+      <Container width={width} height={height}>
+        <canvas ref={bimCanvas} id={canvasID} width={width} height={height} />
+        {navCubeSettings ? (
+          <canvas
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 0,
+            }}
+            id={navCubeSettings.canvasId}
+            width={navCubeSettings.canvasWidth}
+            height={navCubeSettings.canvasHeight}
+          />
+        ) : null}
+
+        {isDev && (
+          <>
+            <DevPanel isDevPanelVisible={isDevPanelVisible}>
+              <TreeViewContainer id="treeViewContainer" />
+              <Buttons>
+                <IconButton
+                  type="button"
+                  id="take-screenshot"
+                  className="btn btn-primary"
+                  onClick={takeScreenshot}
+                >
+                  <PhotoCameraIcon />
+                </IconButton>
+                <IconButton onClick={downloadBCF}>
+                  <SaveIcon />
+                </IconButton>
+              </Buttons>
+            </DevPanel>
+            <IconButton
+              onClick={toggleDevPanel}
+              style={{ position: 'absolute', left: 10, top: 10 }}
+            >
+              {isDevPanelVisible ? <CloseIcon /> : <MenuIcon />}
+            </IconButton>
+          </>
+        )}
+      </Container>
+    );
+  };
 
   return ModelViewer;
 };
