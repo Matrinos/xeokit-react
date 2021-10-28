f6bd897efbba612dfe3a0244692dba4a0f4b113a
Jiacheng Jiang <ajax15601560911@gmail.com>
2021-10-28 15:12:51 +0800

    Fixed build issue

--- src/Viewers.ts
+++ src/Viewers.ts
@@ -1,5 +1,4 @@
 import { makeViewer } from './Viewer';
 import { GLTFLoaderPlugin, XKTLoaderPlugin } from '@tuxmart/xeokit-sdk';
-export { EditorViewer } from './EditorViewer';
 export const GLTFViewer = makeViewer(GLTFLoaderPlugin);
 export const XKTViewer = makeViewer(XKTLoaderPlugin);
