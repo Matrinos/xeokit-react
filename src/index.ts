915336892c3bcceebe2bbd64a4ab4aa685f3aaa6
Jiacheng Jiang <ajax15601560911@gmail.com>
2021-11-19 10:18:56 +0800

    Exporting Model type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { GLTFViewer, XKTViewer, } from './Viewers';
-export { getAABBCenter, get2dFrom3d, drawAABB } from './utils';
-export { BCFViewpointsJSON } from './Viewer';
+export { drawAABB, get2dFrom3d, getAABBCenter } from './utils';
+export type { BCFViewpointsJSON, Model } from './Viewer';
+export { GLTFViewer, XKTViewer } from './Viewers';
+
